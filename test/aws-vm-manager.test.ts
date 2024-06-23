ab34e8262c6c6fdaf43823db79d96e2b050907d1
AgentGino <himakar@qwik.tools>
2024-06-22 23:11:28 -0300

    Initial commit

---
+++ test/aws-vm-manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsVmManager from '../lib/aws-vm-manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-vm-manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsVmManager.AwsVmManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
